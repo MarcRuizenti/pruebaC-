bdea9cad8bf3e174c394a8b04b49c4e7dacfcf73
Marc Ruiz <marc.ruiz.garcia@enti.cat>
2023-11-24 10:49:45 +0100

    Priemer archivo

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main (int argc, char** argv){
+
+	//std::cout << argc << std::endl;
+		
+	if (argc > 1){
+		std::cout << "Hola " << argv[1] << std::endl;
+	}
+	else {
+		std::cout << "Introduce un numero" << std::endl;
+	}
+
+	return 0;
+}
